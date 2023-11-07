9b0cab857be1d7f87f161ff2e97e85c3843302cb
Mathisvoillot <150149789+Mathisvoillot@users.noreply.github.com>
2023-11-07 15:36:45 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,1 +1,17 @@
 
+int option;
+
+int main(){
+
+
+        printf("Menu principal : \n\n");
+        printf("1)Regles du jeu\n");
+        printf("2)Lancer un nouveau Jeu a partir du niveau 1\n");
+        printf("3)Charger une partie\n");
+        printf("4)Mot de passe\n");
+        printf("5)Scores\n");
+        printf("6)Quitter\n\n");
+        printf("choisisssez une option.");
+        scanf("%d",&option);
+
+}
