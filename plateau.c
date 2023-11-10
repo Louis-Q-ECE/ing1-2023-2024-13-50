5bf34127ae2347f3a97949f053ab8587ac159e10
Louis-Q-ECE <150018019+Louis-Q-ECE@users.noreply.github.com>
2023-11-10 14:12:39 +0100

    Update plateau.c

--- plateau.c
+++ plateau.c
@@ -1,30 +1,27 @@
-#include <stdio.h>
-#include <windows.h>
-#include <time.h>
-#include <stdlib.h>
-#include <math.h>
-#include <conio.h>
+
+int main () {
+    int tab[12][22];
 
 
-int main (){
-    int matrice[12][22];
 
     // Initialisation de la matrice
     for (int a = 0; a < 12; a++) {
         for (int b = 0; b < 22; b++) {
             if (a == 0 || a == 11) {
-                matrice[a][b] = 1;  //  les bordures du toit et du plancher
-            }else if (b == 0 || b == 21) {
-                matrice[a][b] = 2; // les bordures des murs
-            }else if (a==1 && b==1 || a==10 && b==1 || a==1 && b==20 || a==10 && b==20){
-                matrice[a][b] = 3;
-            }else if (a==2 && b==4 || a==2 && b==5 || a==2 && b==6 || a==2 && b==6){
-                matrice[a][b] = 4;
-            }else if (a==6 && b==11 || a==7 && b==11 || a==8 && b==11 || a==8 && b==11){
-                matrice[a][b] = 5;
-            }
-            else {
-                matrice[a][b] = 0;  // l'intérieur
+                tab[a][b] = 1;  //  les bordures du toit et du plancher
+            } else if (b == 0 || b == 21) {
+                tab[a][b] = 2; // les bordures des murs
+            } else if (a == 1 && b == 1 || a == 10 && b == 1 || a == 1 && b == 20 || a == 10 && b == 20) {
+                tab[a][b] = 3;
+            } else if (a == 2 && b == 4 || a == 2 && b == 5 || a == 2 && b == 6 || a == 2 && b == 6) { // bloc poussable
+                tab[a][b] = 4;
+            } else if (a == 6 && b == 11 || a == 7 && b == 11 || a == 8 && b == 11 ||
+                       a == 8 && b == 11) { //bloc cassable
+                tab[a][b] = 5;
+            } else if (a == 8 && b == 12 || a == 9 && b == 12 || a == 5 && b == 4 || a == 5 && b == 5) { //bloc cassable
+                tab[a][b] = 6;
+            } else {
+                tab[a][b] = 0;  // l'intérieur
             }
         }
     }
@@ -41,34 +38,63 @@ int main (){
                 if (a == y && b == x) {
                     printf("%c", character);
                 } else {
-                    if (matrice[a][b] == 1) {
+                    if (tab[a][b] == 1) {
                         printf("-");
-                    } else if (matrice[a][b] == 2) {
+                    } else if (tab[a][b] == 2) {
                         printf("|");
-                    } else if (matrice[a][b] == 3) {
+                    } else if (tab[a][b] == 3) {
                         printf("A");
-                    } else if (matrice[a][b] == 4) {
+                    } else if (tab[a][b] == 4) {
                         printf("#");
-                    }else if (matrice[a][b] == 5) {
+                    } else if (tab[a][b] == 5) {
                         printf("0");
-                    }else {
+                    } else if (tab[a][b] == 6) {
+                        printf("&");
+                    } else {
                         printf(" ");
                     }
                 }
             }
             printf("\n");
         }
+
+
         // code pour deplacer le personnage
         char key = getch();
 
         if (key == 'z' && y > 1) {
+            if (tab[y - 1][x] == 3) {
+                tab[y - 1][x] = 0; // Fait disparaître le "A" si le personnage passe dessus
+                if (tab[x][y] == 0) {
+                    printf(" ");
+                }
+            }
             y--;
         } else if (key == 's' && y < 10) {
+            if (tab[y + 1][x] == 3) {
+                tab[y + 1][x] = 0; // Fait disparaître le "A" si le personnage passe dessus
+                if (tab[x][y] == 0) {
+                    printf(" ");
+                }
+            }
             y++;
         } else if (key == 'q' && x > 1) {
+            if (tab[y][x - 1] == 3) {
+                tab[y][x - 1] = 0; // Fait disparaître le "A" si le personnage passe dessus
+                if (tab[x][y - 1] == 0) {
+                    printf(" ");
+                }
+            }
             x--;
         } else if (key == 'd' && x < 20) {
+            if (tab[y][x + 1] == 3) {
+                tab[y][x + 1] = 0; // Fait disparaître le "A" si le personnage passe dessus
+                if (tab[x][y] == 0) {
+                    printf(" ");
+                }
+            }
             x++;
         }
+
     }
 }
