cd80b9937276a8b2888974ef352ad90f29c75a2e
Mathisvoillot <150149789+Mathisvoillot@users.noreply.github.com>
2023-11-13 00:26:57 +0100

    Update plateau.c

--- plateau.c
+++ plateau.c
@@ -133,8 +133,7 @@ int main() {
             // Code pour déplacer le personnage
                 char key = getch();
                 if (key == 'z' && y > 1) {
-                    // Vérifiez si la case de destination n'est pas un bloc poussable
-                    if (tab[y - 1][x] != 4 && tab[y - 1][x] != 10 && tab[y - 1][x] != 11 && tab[y - 1][x] != 12) {
+                    
                         if (tab[y - 1][x] == 3 || tab[y - 1][x] == 5) {
                             tab[y - 1][x] = 0; // Fait disparaître l'oiseau
                             if (tab[x][y] == 0) {
@@ -142,20 +141,18 @@ int main() {
                                 oiseauxRestant--;
                                 win();
                             }
-                        }
                     } else if (tab[y - 1][x] == 4) {
                         tab[y - 1][x] = 0;
                         tab[y - 2][x] = 4; // Déplace le bloc une case aprés dans la direction du déplacement
                         if (tab[x][y] == 0) {
                             printf(" ");
                         } else if (tab[x][y] == 4) {
-                            printf("#");
+                            printf("↑");
                         }
                     }
                     y--;
                 } else if (key == 's' && y < 10) {
-                    // Vérifiez si la case de destination n'est pas un bloc poussable
-                    if (tab[y + 1][x] != 4 && tab[y + 1][x] != 10 && tab[y + 1][x] != 11 && tab[y + 1][x] != 12) {
+                    
                         if (tab[y + 1][x] == 3 || tab[y + 1][x] == 5) {
                             tab[y + 1][x] = 0; // Fait disparaître l'oiseau
                             if (tab[x][y] == 0) {
@@ -163,20 +160,19 @@ int main() {
                                 oiseauxRestant--;
                                 win();
                             }
-                        }
+                        
                     } else if (tab[y + 1][x] == 10) {
                         tab[y + 1][x] = 0;
                         tab[y + 2][x] = 10; // Déplace le bloc une case après dans la direction du déplacement
                         if (tab[x][y] == 0) {
                             printf(" ");
                         } else if (tab[x][y] == 10) {
-                            printf("#");
+                            printf("↓");
                         }
                     }
                     y++;
                 } else if (key == 'q' && x > 1) {
-                    // Vérifiez si la case de destination n'est pas un bloc poussable
-                    if (tab[y][x - 1] != 4 && tab[y][x - 1] != 10 && tab[y][x - 1] != 11 && tab[y][x - 1] != 12) {
+                    
                         if (tab[y][x - 1] == 3 || tab[y][x - 1] == 5) {
                             tab[y][x - 1] = 0; // Fait disparaître l'oiseau
                             if (tab[x][y - 1] == 0) {
@@ -184,20 +180,19 @@ int main() {
                                 oiseauxRestant--;
                                 win();
                             }
-                        }
+                        
                     } else if (tab[y][x - 1] == 11) {
                         tab[y][x - 1] = 0;
                         tab[y][x - 2] = 11; // Déplace le bloc une case après dans la direction du déplacement
                         if (tab[x][y] == 0) {
                             printf(" ");
                         } else if (tab[x][y] == 11) {
-                            printf("#");
+                            printf("←");
                         }
                     }
                     x--;
                 } else if (key == 'd' && x < 20) {
-                    // Vérifiez si la case de destination n'est pas un bloc poussable
-                    if (tab[y][x + 1] != 4 && tab[y][x + 1] != 10 && tab[y][x + 1] != 11 && tab[y][x + 1] != 12) {
+                
                         if (tab[y][x + 1] == 3 || tab[y][x + 1] == 5) {
                             tab[y][x + 1] = 0; // Fait disparaître l'oiseau
                             if (tab[x][y] == 0) {
@@ -212,7 +207,7 @@ int main() {
                         if (tab[x][y] == 0) {
                             printf(" ");
                         } else if (tab[x][y] == 12) {
-                            printf("#");
+                            printf("→");
                         }
                     }
                     x++;
