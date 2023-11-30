30a9175edb76bbfff2fb7995eea0acb091286ccc
Louis-Q-ECE <150018019+Louis-Q-ECE@users.noreply.github.com>
2023-11-30 14:17:06 +0100

    Update and rename plateau test to main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+
+#include "bibl.h"
+#include <stdio.h>
+int main() {
+    int ChoixBip = 2;
+
+    do {
+        printf("Voulez-vous du son ? \n");
+        printf("\n0 = muet    1 = son active \n");
+        scanf("%d", &ChoixBip);
+        EffacerEcran();
+    } while (ChoixBip != 0 && ChoixBip != 1);
+    menu(ChoixBip);
+}
