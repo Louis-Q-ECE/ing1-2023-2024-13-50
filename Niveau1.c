1b9680291661d30467aff135ed1fe983f8d1fe1e
AdrienGillet <150023726+AdrienGillet@users.noreply.github.com>
2023-12-03 15:40:20 +0100

    Update Niveau1.c

--- Niveau1.c
+++ Niveau1.c
@@ -1,3 +1,26 @@
+#include "niveau.3.h"
+#include <stdio.h>
+#include <windows.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <conio.h>
+
+
+void pause(){
+    printf("\n\n Le jeu est en pause, appuyez sur 'r' pour reprendre la partie\n\n");
+    char key;
+    do {
+        key = getch();
+    } while (key != 'r'); // Attendre que la touche 'r' soit pressée pour reprendre le jeu
+    printf("\nLe jeu reprend...\n");
+    // Ajoutez ici le code pour reprendre le jeu après la pause
+}
+struct TimerData {
+    int temps;
+    int *stopTimerPtr;
+    time_t startTime;
+};
+
 void nv1 () {
     int stopTimer = 0;
     int oiseauxRestant=4;
